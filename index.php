f59acf3d9bf8949142c46a515a5766a5e00f011e
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-09-07 14:07:56 -0500

    Add files via upload
    teacher drafts

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php require_once("indexHeader.php")?>
 
-<body>
+    
     <h1>
         my website test
     </h1>
@@ -8,8 +8,23 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
 
+    <div>
+
+
+        <form action="handlePost.php" method="post">
+            Name: <input type="text" name="name"><br>
+            School: <input type="text" name="School"><br>
+            FavoriteClass: <input type="text" name="FavoriteClass"><br>
+            <input type="submit">
+        </form>
+
+
+    </div>
+
+
 
 
 </body>
+
 
 
