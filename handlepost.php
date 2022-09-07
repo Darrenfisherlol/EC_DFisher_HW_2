c4418ee1db86b712fcfe0ca2fcf14c8c1166a8bb
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-09-07 14:15:12 -0500

    Add files via upload
    tester 4

--- handlepost.php
+++ handlepost.php
@@ -1,3 +1,6 @@
+<?php require_once("indexHeader.php")?>
+
+
 <!doctype html>
 <html lang="en">
     <head>
@@ -6,17 +9,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
-        <title>Darren Fisher HW 1 Ecommerce</title>
+        <title>Darren Fisher HW 2 Ecommerce</title>
     </head>
+    
     <body>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     
-        <div id="divOne"> 
-            Hello, world!
-            </br>
-            Welcoem to Page one!
-        </div>
-        
         <!-- Post echo php -->
         <div>       
             Hi <?php echo $_POST["name"]; ?><br>
