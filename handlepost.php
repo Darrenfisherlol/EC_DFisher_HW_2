fa08fcc93a6b5763daa9e4d95339a8f8771689d0
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-09-07 14:23:58 -0500

    Add files via upload

--- handlepost.php
+++ handlepost.php
@@ -1,26 +1,61 @@
 <?php require_once("indexHeader.php")?>
 
 
-<!doctype html>
-<html lang="en">
-    <head>
-
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-
-        <title>Darren Fisher HW 2 Ecommerce</title>
-    </head>
-
-    <body>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+        <style>
+            #divOne {
+                background-color: azure;
+                height: 75px;
+                font-size: 50px;
+                line-height: 100px;
+                float: left;
+                width: 100%;
+            }
+            #divTwo{
+                background-color: lightskyblue;
+                height: 100px;
+                text-align: center;
+                line-height: 100px;
+                font-size: 50px;
+                float: left;
+                width: 100%;
+            }
+            #divThree{
+                background-color: lightblue;
+                height: 200px;
+                text-align: center;
+                line-height: 200px;
+                float: left;
+                width: 100%;
+            }
+        </style>
     
         <!-- Post echo php -->
-        <div>       
-            Hi <?php echo $_POST["name"]; ?><br>
-            Your school is: <?php echo $_POST["School"]; ?>
-            Your favorite class is: <?php echo $_POST["FavoriteClass"]; ?>            
+       
+    <div class="divOne">
+            <div class="divTwo">
+                Div Two
+            </div>
+            <div class="divTwo">
+                <div>       
+                    Hi <?php echo $_POST["name"]; ?><br>
+                    Your school is: <?php echo $_POST["School"]; ?>
+                    Your favorite class is: <?php echo $_POST["FavoriteClass"]; ?>            
+                </div>
+            </div>
+            <div class="divTwo">
+                div 2 class three 3
+            </div>
+            <div class="divThree">
+                1 1
+            </div>
+            <div class="divThree">
+                2 2
+            </div>
         </div>
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     </body>
 </html>
 
