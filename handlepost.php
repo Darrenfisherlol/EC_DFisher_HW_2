c6612dbbdf374e8482e785dbcdd278cde45036f3
Darren Fisher <45086615+Darrenfisherlol@users.noreply.github.com>
2022-09-26 13:35:54 -0500

    Add files via upload
    new commit 1

--- handlepost.php
+++ handlepost.php
@@ -1,7 +1,5 @@
 <?php require_once("indexHeader.php")?>
 
-
-
         <!-- Post echo php -->
        
     <div class="divOne">
@@ -10,23 +8,22 @@
             <div class="divTwo">
                 <div>       
                     Hi <?php echo $_POST["name"]; ?><br>
-                    Your school is: <?php echo $_POST["School"]; ?><br>
+
+                    <?php
+                        function getClass() {
+                        ?>
+                        Your school is: <?php echo $_POST["School"]; ?><br>
+                        <?php
+                        }
+                        getClass(); 
+                    ?>
+                    <?php echo "FavoriteClass" . $_GET['FavoriteClass'] . " at " . $_GET['web']; ?>
                     Your favorite class is: <?php echo $_POST["FavoriteClass"]; ?>            
                 </div>
             </div>
            
         </div>
 
-
-
-       <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script> -->
     </body>
 </html>
-
-
-
-
-
-
-
 
